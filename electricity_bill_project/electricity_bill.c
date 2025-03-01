d460b78bda6cd7442d079f12ba7518ae9da5204f
Midou-1 <jhon.midou@gmail.com>
2025-03-02 00:21:14 +0100

    Update electricity_bill.c

--- electricity_bill_project/electricity_bill.c
+++ electricity_bill_project/electricity_bill.c
@@ -4,9 +4,19 @@
 
 int main() {
     float kWh, total_cost;
-    printf("Enter electricity consumption (in kWh): ");
-    scanf("%f", &kWh);
+
+    do {
+        printf("Enter electricity consumption (in kWh): ");
+        scanf("%f", &kWh);
+
+        if (kWh < 0) {
+            printf("\n\n\nA7ki Mliiih\n\n\n");
+        }
+
+    } while (kWh < 0);
+
     total_cost = kWh * PRICE_PER_KWH;
     printf("\n\n\nTotal electricity bill: %.2f DZD\n\n\n", total_cost);
+
     return 0;
 }
